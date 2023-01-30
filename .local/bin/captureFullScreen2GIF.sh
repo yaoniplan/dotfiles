9fcbc8e7e80e780a2e19b162abef2542446f7221
yaoniplan <yaoniplan@gmail.com>
2023-01-30 11:10:49 +0800

    Send a string to the clipboard

--- .local/bin/captureFullScreen2GIF.sh
+++ .local/bin/captureFullScreen2GIF.sh
@@ -1,15 +1,20 @@
 #! /bin/sh
 
 # Capture full screen to GIF
-# Dependencies: xwininfo, gawk, byzanz, paplay, an audio file
+# Dependencies: xwininfo, gawk, byzanz, paplay, an audio file, xclip
 # Byzanz refer to https://askubuntu.com/questions/107726/how-to-create-animated-gif-images-of-a-screencast/201018#201018
 
 recordSeconds=10
 widthOfFullScreen=$(xwininfo -root | gawk '/Width/ { print $2 }')
 heightOfFullScreen=$(xwininfo -root | gawk '/Height/ { print $2 }')
-#fileName="${HOME}/test/assets/`date +%F_%T`.gif"
 storageLocation=$HOME
 fileName="`date +%F_%T`.gif"
+
+# Write a function that sends a string to the clipboard
+sendToTheClipboard() {
+    echo "![$fileName](../assets/$fileName)" | xclip -selection clipboard
+}
+
 # Write a sound notification
 soundNotification() {
     paplay /usr/share/sounds/alsa/Noise.wav
@@ -20,4 +25,5 @@ byzanz-record --duration=$recordSeconds \
     --x=0 --y=0 \
     --width=$widthOfFullScreen --height=$heightOfFullScreen \
     "$storageLocation/$fileName"
+sendToTheClipboard
 soundNotification
