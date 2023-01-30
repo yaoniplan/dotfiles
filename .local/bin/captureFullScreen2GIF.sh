71dbc2e658d89d27f1cae930dac18d0f1409211f
yaoniplan <yaoniplan@gmail.com>
2023-01-30 10:03:32 +0800

    Add sound notification

--- .local/bin/captureFullScreen2GIF.sh
+++ .local/bin/captureFullScreen2GIF.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Capture full screen to GIF
-# Dependencies: xwininfo, gawk, byzanz
+# Dependencies: xwininfo, gawk, byzanz, paplay, an audio file
 # Refer to https://askubuntu.com/questions/107726/how-to-create-animated-gif-images-of-a-screencast/201018#201018
 # https://serverfault.com/questions/532559/bash-script-count-down-5-minutes-display-on-single-line
 # $countdownSeconds refer to https://unix.stackexchange.com/questions/246259/write-a-script-that-reads-a-number-and-counts-down-to-0
@@ -13,6 +13,10 @@ heightOfFullScreen=$(xwininfo -root | gawk '/Height/ { print $2 }')
 fileName="$HOME/`date +%F_%T`.gif"
 # Write a countdown here
 countdownSeconds=2
+# Write a sound and display notification
+soundNotification() {
+    paplay /usr/share/sounds/alsa/Noise.wav
+}
 while [[ $countdownSeconds > 0 ]]
 do
     echo $countdownSeconds
@@ -20,7 +24,9 @@ do
     sleep 1
 done
 
+soundNotification
 byzanz-record --verbose --duration=$recordSeconds \
     --x=0 --y=0 \
     --width=$widthOfFullScreen --height=$heightOfFullScreen \
     $fileName
+soundNotification
