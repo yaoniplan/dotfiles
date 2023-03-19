9c15d1e1d78342be9a5ae657a88e8961539bcc34
yaoniplan <yaoniplan@gmail.com>
2023-03-19 22:24:42 +0800

    Use two branches instead of two repositories

--- .local/bin/autoGitPush.sh
+++ .local/bin/autoGitPush.sh
@@ -1,20 +1,20 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 # Type "echo `date +%F_%T`" in terminal to display output
 # Refer to https://unix.stackexchange.com/questions/395933/how-to-check-if-the-current-time-is-between-2300-and-0630
 
 currentTime=$(date +%H:%M)
-dayOfWeek=$(date +%A)
-if [[ $dayOfWeek == "Monday" && $currentTime > "16:59" && $currentTime < "17:01" ]]
+dayOfWeek=$(date +%u)
+if (( ($dayOfWeek / 2) * 2 == $dayOfWeek ))
 then
     cd $HOME/note/
     git add --all
     git commit -m "Update at `date +%F_%T`"
-    git push
+    git push origin development:master
 fi
 
-cd $HOME/test/
-cat $(ls /home/yaoniplan/test/journals/*.md | sort -r) > /home/yaoniplan/test/.github/README.md
+cd $HOME/note/
+cat $(ls /home/yaoniplan/note/journals/*.md | sort -r) > /home/yaoniplan/note/.github/README.md
 git add --all
 git commit -m "Update at `date +%F_%T`"
-git push
+git push origin development
