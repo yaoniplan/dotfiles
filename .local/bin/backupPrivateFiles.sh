15d183cf39fbd542cf75584f461bd40ca43c53ca
yaoniplan <yaoniplan@gmail.com>
2023-03-22 11:57:25 +0800

    Backup between two computers using rsync tool

--- .local/bin/backupPrivateFiles.sh
+++ .local/bin/backupPrivateFiles.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
 # Set the source and destination directories
-sourceDirectory="/mnt/Toshiba/testGrowthRecord"
-destinationDirectory="/mnt/test/backup"
-
-# Create the destination directory if it does not exist
-if [ ! -d $destinationDirectory ]; then
-    mkdir $destinationDirectory
-fi
+remoteUser="yaoniplan"
+remoteHost="192.168.10.105"
+sourceDirectory="/mnt/Toshiba/growthRecord"
+destinationDirectory="/mnt/backupPrivateFiles"
 
 # Set the current date as the backup folder name
-backupFolder="$(date +%F_%T)"
+backupFolder="$(date +%F)"
 
-# Create the backup folder within the destination directory
-mkdir "$destinationDirectory/$backupFolder"
+# Create the destination directory if it does not exist
+mkdir -p $destinationDirectory/$backupFolder
 
 # Copy files from the source directory to the backup folder
-cp -r $sourceDirectory/* "$destinationDirectory/$backupFolder"
+rsync -av -e ssh $remoteUser@$remoteHost:"$sourceDirectory/" "$destinationDirectory/$backupFolder/"
 
 # Print a success message
-echo "Backup completed successfully!"
+printf "Backup completed successfully!\n"
