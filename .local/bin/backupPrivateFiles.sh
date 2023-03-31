831ac0a97df9bb4d80f7fadcdc9e0605e9e19542
yaoniplan <yaoniplan@gmail.com>
2023-03-31 16:53:45 +0800

    Shorten some variables name

--- .local/bin/backupPrivateFiles.sh
+++ .local/bin/backupPrivateFiles.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-# Set the source and destination directories
+# Set variables
 remoteUser="yaoniplan"
 remoteHost="192.168.10.105"
-sourceDirectory="/mnt/Toshiba/growthRecord"
-destinationDirectory="/mnt/backupPrivateFiles"
-
-# Set the current date as the backup folder name
+sourceDir="/mnt/Toshiba/growthRecord"
+destinationDir="/mnt/backupPrivateFiles"
 backupFolder="$(date +%F)"
 
 # Create the destination directory if it does not exist
-mkdir -p $destinationDirectory/$backupFolder
+mkdir -p $destinationDir/$backupFolder
 
 # Copy files from the source directory to the backup folder
-rsync -av -e ssh $remoteUser@$remoteHost:"$sourceDirectory/" "$destinationDirectory/$backupFolder/"
-
-# Print a success message
-printf "Backup completed successfully!\n"
+rsync -av $remoteUser@$remoteHost:"$sourceDir/" "$destinationDir/$backupFolder/"
