f575f66da68ee91a0fa68e3c0dcf86d0d720e816
yaoniplan <yaoniplan@gmail.com>
2023-04-03 10:38:53 +0800

    Add .local/bin/getIPInformation.sh

---
+++ .local/bin/getIPInformation.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Get the public IP address, city and country information
+IPAddress=$(curl -s https://ipinfo.io/ip)
+city=$(curl -s https://ipinfo.io/${IPAddress}/city)
+country=$(curl -s https://ipinfo.io/${IPAddress}/country)
+
+# Generage the output result
+outputResult="Your IP address in ${city}, ${country} is ${IPAddress}."
+
+# Output the result using notify-send if available, or echo otherwise
+if command -v notify-send > /dev/null
+then
+    notify-send "$outputResult"
+else
+    echo "$outputResult"
+fi
