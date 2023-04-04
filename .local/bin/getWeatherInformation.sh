f71c83291046781f83c5ba01f004ab6a5e37da96
yaoniplan <yaoniplan@gmail.com>
2023-04-04 08:01:10 +0800

    Add a way to open in terminal

--- .local/bin/getWeatherInformation.sh
+++ .local/bin/getWeatherInformation.sh
@@ -6,14 +6,11 @@ URL="https://www.google.com/search?q=$(echo $city | sed 's/,/+/g')+weather"
 
 # Create a temporary directory to store the file
 temporaryDir=$(mktemp --directory)
-
-# Download the weather image and open it in a web browser
 wget -P "$temporaryDir" "https://wttr.in/$city.png"
-chromium "$temporaryDir/$city.png"
 
-# Wait for 5 seconds and open the weather forecast in a web browser
-sleep 5s
-chromium "$URL"
-
-# Remove the temporary directory and its contents
-rm -rf "$temporaryDir"
+# Output the result using Chromium if available, otherwise using curl
+if ! chromium "$temporaryDir/$city.png" "$URL" 2>/dev/null
+then
+    curl "wttr.in/$city" | less -RS
+    sleep 5s && rm -rf "$temporaryDir"
+fi
