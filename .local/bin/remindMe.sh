599de934d5767ea692cf5332242d356d352ca4a9
yaoniplan <yaoniplan@gmail.com>
2023-04-09 16:17:40 +0800

    Add a file path variable and remind twice

--- .local/bin/remindMe.sh
+++ .local/bin/remindMe.sh
@@ -4,4 +4,10 @@
 # Usage: `remindMe.sh 3m &`
 # Dependencies: sleep, paplay, an audio file, notify-send
 
-sleep $1; paplay /usr/share/sounds/alsa/Noise.wav & notify-send "Time is up" &
+filePath="/usr/share/sounds/alsa/doorbell.mp3"
+sleep $1
+notify-send "Time is up" &
+for times in {1..2}
+do
+    paplay $filePath
+done
