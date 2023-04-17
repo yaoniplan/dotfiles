f85962f10c370e2aec768fccf3cbb3b36bc1ad06
yaoniplan <yaoniplan@gmail.com>
2023-04-17 14:48:17 +0800

    Make it easy to understand

--- .local/bin/playWithMPV.sh
+++ .local/bin/playWithMPV.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-# Check if the clipboard is empty or not
-URLOfVideo=$(xclip -o)
-if [ -z "$URLOfVideo" ]
-then
-    echo "Error: No text found in clipboard"
-    exit 1
-fi
-
-# Play video with mpv
-mpv --speed=2 --fs=yes $(xclip -o)
+mpv --speed=2 --fullscreen $(xclip -o)
