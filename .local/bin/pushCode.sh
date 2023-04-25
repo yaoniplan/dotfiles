f8d9c38aaaa9544241d4e798172918aae246c4a1
yaoniplan <yaoniplan@gmail.com>
2023-04-25 14:55:56 +0800

    Change source file to yaoniplan.txt

--- .local/bin/pushCode.sh
+++ .local/bin/pushCode.sh
@@ -3,7 +3,7 @@
 # Set variables
 repoDir="$HOME/note"
 journalsDir="$repoDir/journals"
-readmeFile="$repoDir/.github/README.md"
+yaoniplanFile="$repoDir/assets/yaoniplan.txt"
 
 # Push changes to master branch every odd day at 5pm
 if [[ $(( $(date +%-j) % 2)) -eq 1 && $(date +%H:%M) == "17:00" ]]
@@ -11,8 +11,8 @@ then
     git -C "$repoDir" push origin development:master
 fi
 
-# Concatenate all journals files and write to README file
-cat $(ls "$journalsDir"/*.md | sort -r) > "$readmeFile"
+# Concatenate all journals files and write to yaoniplan file
+cat $(ls "$journalsDir"/*.md | sort -r) > "$yaoniplanFile"
 
 # Commit and push changes to development branch
 git -C "$repoDir" add --all
