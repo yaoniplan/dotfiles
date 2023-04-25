5f9746670647dd293e0c55d5495ff93cdd8ad403
yaoniplan <yaoniplan@gmail.com>
2023-04-25 15:35:01 +0800

    Use default web browser or curl

--- .local/bin/getWeatherInformation.sh
+++ .local/bin/getWeatherInformation.sh
@@ -2,17 +2,11 @@
 
 # Set variables
 city="Dongxiang,Fuzhou,Jiangxi,China"
-URL="https://www.google.com/search?q=$(echo $city | sed 's/,/+/g')+weather"
+URL="https://www.google.com/search?q="$(echo "$city" | sed 's/,/+/g')"+weather"
 
-# Output the result using Chromium if available, otherwise using curl
-if command -v chromium &>/dev/null; then
-    chromium "$URL" 2>/dev/null
-    temporaryDir=$(mktemp --directory)
-    wget -P "$temporaryDir" "https://wttr.in/$city.png"
-    sleep 5s
-    chromium "$temporaryDir/$city.png"
-    sleep 5s
-    rm -rf "$temporaryDir"
+# Use default web browser or curl
+if command -v xdg-open &>/dev/null; then
+    xdg-open "$URL"
 else
-    curl "wttr.in/$city" | less -RS
+    curl "wttr.in/"$city"" | less -RS
 fi
