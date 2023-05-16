10c69560652be2d78a7afd9cf11dba0977fe6491
yaoniplan <yaoniplan@gmail.com>
2023-05-16 09:35:30 +0800

    Detect if a command exists

--- .local/bin/getIPInformation.sh
+++ .local/bin/getIPInformation.sh
@@ -4,12 +4,11 @@
 IPAddress=$(curl -s https://ipinfo.io/ip)
 city=$(curl -s https://ipinfo.io/${IPAddress}/city)
 country=$(curl -s https://ipinfo.io/${IPAddress}/country)
-
-# Generage the output result
 outputResult="Your IP address in ${city}, ${country} is ${IPAddress}."
 
 # Output the result using notify-send if available, or echo otherwise
-if ! notify-send "$outputResult" 2>/dev/null
-then
+if command -v notify-send &>/dev/null; then
+    notify-send "$outputResult"
+else
     echo "$outputResult"
 fi
