50cb44a743f618c740fe3b94b43669c9c4e35478
yaoniplan <yaoniplan@gmail.com>
2023-05-26 23:17:31 +0800

    Keep 30 directories only

--- .local/bin/backupPrivateFiles.sh
+++ .local/bin/backupPrivateFiles.sh
@@ -6,9 +6,16 @@ remoteHost="192.168.10.105"
 sourceDir="/mnt/grow"
 destinationDir="/mnt/grow"
 backupFolder="$(date +%F)"
+numberOfFiles=$(ls -1 "$sourceDir" | wc -l)
+quantityToDelete=$(("$numberOfFiles" - 30))
 
 # Create the destination directory if it does not exist
 mkdir -p $destinationDir/$backupFolder
+
+# Keep 30 directories only
+if [[ "$numberOfFiles" -gt 30 ]]; then
+    ls -1dr "$destinationDir"/* | tail -"$quantityToDelete" | xargs rm -rf
+fi
 
 # Copy files from the source directory to the backup folder
 rsync -av --exclude="*.iso" $remoteUser@$remoteHost:"$sourceDir/" "$destinationDir/$backupFolder/"
