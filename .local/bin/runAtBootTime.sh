7cd02c60ce0756e7c9a7357596a3a65d76b4d26f
yaoniplan <yaoniplan@gmail.com>
2023-05-28 10:52:01 +0800

    Remove a command about yakuake

--- .local/bin/runAtBootTime.sh
+++ .local/bin/runAtBootTime.sh
@@ -8,10 +8,6 @@ if command -v redshift &>/dev/null; then
     redshift -O 1500 &>/dev/null &
 fi
 
-if command -v yakuake &>/dev/null; then
-    yakuake &>/dev/null &
-fi
-
 clash &>/dev/null &
 rclone mount aliyundrive:/ /mnt/aliyundrive/ &>/dev/null &
 timerOfTomato.sh &>/dev/null &
