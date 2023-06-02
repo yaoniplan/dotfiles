1f95f2b395f512f2e2f7e1363deced42f42c7bc4
yaoniplan <yaoniplan@gmail.com>
2023-06-02 14:02:16 +0800

    Keep 15 directories only instead of 30

--- .local/bin/backupPrivateFiles.sh
+++ .local/bin/backupPrivateFiles.sh
@@ -7,13 +7,13 @@ sourceDir="/mnt/grow"
 destinationDir="/mnt/grow"
 backupFolder="$(date +%F)"
 numberOfFiles=$(ls -1 "$sourceDir" | wc -l)
-quantityToDelete=$(("$numberOfFiles" - 30))
+quantityToDelete=$(("$numberOfFiles" - 15))
 
 # Create the destination directory if it does not exist
 mkdir -p $destinationDir/$backupFolder
 
 # Keep 30 directories only
-if [[ "$numberOfFiles" -gt 30 ]]; then
+if [[ "$numberOfFiles" -gt 15 ]]; then
     ls -1d "$destinationDir"/* | head -"$quantityToDelete" | xargs rm -rf
 fi
 
