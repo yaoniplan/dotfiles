5ecb13c2057253cd8b8df2d6aa571fc19cb6d649
yaoniplan <yaoniplan@gmail.com>
2023-06-11 15:49:50 +0800

    Add condition judgment

--- .local/bin/runAtBootTime.sh
+++ .local/bin/runAtBootTime.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
-if command -V tilda &>/dev/null && ! pgrep tilda &>/dev/null; then
+if commmand -v feh &>/dev/null; then
+    feh $HOME/note/assets/dark.jpg &>/dev/null &
+fi
+
+if command -v tilda &>/dev/null && ! pgrep tilda; then
     tilda --command tmux &>/dev/null &
 fi
 
-if command -v redshift &>/dev/null; then
-    redshift -O 1500 &>/dev/null &
+if command -v clash && ! pgrep clash; then
+    clash &>/dev/null &
 fi
 
-clash &>/dev/null &
-rclone mount yaoniplan:/ /mnt/yaoniplan/ &>/dev/null &
-timerOfTomato.sh &>/dev/null &
+if command -v rclone && ! pgrep rclone; then
+    rclone mount yaoniplan:/ /mnt/yaoniplan --header "Referer:" &>/dev/null &
+fi
+
+if command -v redshift && ! pgrep redshift; then
+    redshift -x; redshift -O 1500 &>/dev/null &
+fi
+
+if ! ps aux | grep -v grep | grep aolaStar; then
+    mpv --shuffle --loop-playlist /mnt/grow/230601aolaStar/ &>/dev/null &
+fi
+
+if ! ps aux | grep -v grep | grep timerOfTomato.sh; then
+    timerOfTomato.sh &>/dev/null &
+fi
