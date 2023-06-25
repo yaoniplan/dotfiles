f66509aeed6c6e6723a43f2f25db193f61612efa
yaoniplan <yaoniplan@gmail.com>
2023-06-25 08:47:54 +0800

    Correct the comment

--- .local/bin/runContainer.sh
+++ .local/bin/runContainer.sh
@@ -6,12 +6,11 @@ searchString="0.0.0.0:8000"
 portInfo=$(docker ps --format "{{.Ports}}" | awk -F'->' '{print $1}')
 containerID=$(docker ps --format "{{.ID}} {{.Ports}}" | awk '/'"$searchString"'/ {print $1}')
 
-# Check if search string is in port info
+# Determine if the container is running or not
 if echo "$portInfo" | grep --quiet "$searchString"; then
     docker stop "$containerID"
 fi
 
-# Determin if the container is running or not
 # Remove images with none tag
 docker images --filter "dangling=true" --quiet | xargs -r docker rmi --force
 
