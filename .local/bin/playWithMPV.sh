e5e7d2b04bd523876959badaf6d5333fb1854c14
yaoniplan <yaoniplan@gmail.com>
2023-06-25 10:12:41 +0800

    Check if there are two parameters

--- .local/bin/playWithMPV.sh
+++ .local/bin/playWithMPV.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# Dependencies:
-# mpv, xsel
+# Dependencies: mpv, yt-dlp, xsel
 
-mpv --speed=2 --fullscreen "$(xsel -ob)"
+if [[ "$#" = 2 ]]; then
+    mpv --speed=2 --fullscreen --mute=yes --sub-file="$1" "$2"
+else
+    mpv --speed=2 --fullscreen --mute=yes $(xsel --output --clipboard)
+fi
