ff4388c722f1a5dcc6445cc3d2e570d06fe0a32f
yaoniplan <yaoniplan@gmail.com>
2023-08-03 12:08:17 +0800

    Set color temperature between 22:00 and 07:00

--- .local/bin/runAtBootTime.sh
+++ .local/bin/runAtBootTime.sh
@@ -16,8 +16,16 @@ if command -v rclone && ! pgrep rclone; then
     rclone mount yaoniplan:/ /mnt/yaoniplan --header "Referer:" &>/dev/null &
 fi
 
-if command -v sct ; then
-    sct 1500
+if command -v sct; then
+    currentTime=$(date +%H:%M)
+
+    if [[ $currentTime > "22:00" ]] || [[ $currentTime < "07:00" ]]; then
+        sct 1500
+        echo "Set color temperature to 1500K between 22:00 and 07:00"
+    else
+        sct
+        echo "Reset to the default color temperature"
+    fi
 fi
 
 if ! ps aux | grep -v grep | grep aolaStar; then
