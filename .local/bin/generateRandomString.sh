f41a5b943eccc638ff4e45c155381bcd3a45bd05
yaoniplan <yaoniplan@gmail.com>
2023-08-03 12:18:19 +0800

    Check if exists a clipboard tool xsel

--- .local/bin/generateRandomString.sh
+++ .local/bin/generateRandomString.sh
@@ -1,8 +1,14 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 # Generate random string
 # At least one uppercase letter, one lowercase letter, one number, and one special symbol
 
 generateRandomString=$(openssl rand -base64 11)
 
-echo $generateRandomString@gmail.com | xclip -selection clipboard
+if command -v xclip; then
+    echo $generateRandomString@gmail.com | xclip -selection clipboard
+elif command -v xsel; then
+    echo $generateRandomString@gmail.com | xsel --input --clipboard
+else
+    echo "xclip or xsel not found"
+fi
