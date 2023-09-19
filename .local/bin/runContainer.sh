ae6b7de6ffe838d6996730efb46d0a09b290674e
yaoniplan <yaoniplan@gmail.com>
2023-09-19 18:07:32 +0800

    Remove containers and images before pushing to Docker Hub

--- .local/bin/runContainer.sh
+++ .local/bin/runContainer.sh
@@ -1,19 +1,26 @@
 #!/usr//bin/env bash
 
 # Set variables
+yourName="yaoniplan"
 yourImage="yaoniplan/note"
 yourTag=$(date +%F) # (e.g. 2023-09-17)
-yourPathIncludingDockerfile="~/note/"
-yourPathIncludingDockerComposeYml="~/.config/note/"
-yourLocalIPAddress="192.168.10.105"
+yourPathIncludingDockerfile="$HOME/note"
+yourPathIncludingDockerComposeYml="$HOME/.config/note"
+
+# Remove existing runing containers
+docker ps --format "{{.ID}} {{.Image}}" | awk '/'"yaoniplan"'/ {print $1}' | xargs -r docker stop {} && docker rm {}
+
+# Remove images with none and yourName tag
+docker images --filter "dangling=true" --quiet | xargs -r docker rmi --force
+docker images | awk '/'"$yourName"'/ {print $3}' | xargs -r docker rmi --force
 
 # Determine whether it is on the local
 if echo $(ip address) | grep --quiet "$yourLocalIPAddress"; then
     # Build and push the Docker image on the local
-    docker build --tag "$yourImage":latest "$yourPathIncludingDockerfile" && \
-    docker push "$yourImage":latest & \
+    docker build --tag "$yourImage":latest "$yourPathIncludingDockerfile"
+    docker push "$yourImage":latest
     docker tag "$yourImage":latest "$yourImage":"$yourTag" && \
-    docker push "$yourImage":"$yourTag"
+    docker push "$yourImage":"$yourTag" &
     ssh yaoniplan 'bash -c "source ~/.local/bin/runContainer.sh"' &
 else
     # Run the Docker container on the server
