cc688c3d64484c2980c63eea48a8dc21525c9221
yaoniplan <yaoniplan@gmail.com>
2023-10-24 17:48:10 +0800

    Add UTF-8 encoding support to display Chinese characters

--- .local/bin/convertMarkdownToHtml.sh
+++ .local/bin/convertMarkdownToHtml.sh
@@ -12,6 +12,7 @@ convertMarkdownToHtml() {
 <!DOCTYPE html>\
 <html>\
 <head>\
+  <meta charset="UTF-8">\
   <meta name="viewport" content="width=device-width, initial-scale=1">\
   <title>yaoniplan</title>\
   <link rel="icon" href="../assets/favicon.png" type="image/png">\
