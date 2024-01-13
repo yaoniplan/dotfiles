98cb2233a825c11d02d7581ba6d98f687508057d
yaoniplan <yaoniplan@gmail.com>
2024-01-14 00:39:10 +0800

    Replace sound notifications with delay seconds

--- .local/bin/captureFullScreen2GIF.sh
+++ .local/bin/captureFullScreen2GIF.sh
@@ -1,29 +1,30 @@
-#! /bin/sh
+#!/bin/bash
 
-# Capture full screen to GIF
-# Dependencies: xwininfo, gawk, byzanz, paplay, an audio file, xclip
-# Byzanz refer to https://askubuntu.com/questions/107726/how-to-create-animated-gif-images-of-a-screencast/201018#201018
 
-recordSeconds=10
-widthOfFullScreen=$(xwininfo -root | gawk '/Width/ { print $2 }')
-heightOfFullScreen=$(xwininfo -root | gawk '/Height/ { print $2 }')
-storageLocation=$HOME
-fileName="`date +%F_%H-%M`.gif"
+source $HOME/.local/bin/master.sh
 
-# Write a function that sends a string to the clipboard
-sendToTheClipboard() {
-    echo -n "![$fileName](../assets/$fileName)" | xclip -selection clipboard
-}
+# Delay before starting
+fileExtension="gif"
+DELAY=3
 
-# Write a sound notification
-soundNotification() {
-    paplay /usr/share/sounds/alsa/Noise.wav
-}
+# Duration and output file
+if [ $# -gt 0 ]; then
+    D="--duration=$@"
+else
+    echo Default recording duration "$DELAY"s to "$fileName".gif
+    D="--duration=10 "$fileName".gif"
+fi
+XWININFO=$(xwininfo)
+read X <<< $(awk -F: '/Absolute upper-left X/{print $2}' <<< "$XWININFO")
+read Y <<< $(awk -F: '/Absolute upper-left Y/{print $2}' <<< "$XWININFO")
+read W <<< $(awk -F: '/Width/{print $2}' <<< "$XWININFO")
+read H <<< $(awk -F: '/Height/{print $2}' <<< "$XWININFO")
 
-soundNotification
-byzanz-record --duration=$recordSeconds \
-    --x=0 --y=0 \
-    --width=$widthOfFullScreen --height=$heightOfFullScreen \
-    "$storageLocation/$fileName"
+echo Delaying $DELAY seconds. After that, byzanz will start
+for (( i=$DELAY; i>0; --i )) ; do
+    echo $i
+    sleep 1
+done
+
+byzanz-record --verbose --delay=0 --x=$X --y=$Y --width=$W --height=$H $D
 sendToTheClipboard
-soundNotification
--- .local/bin/master.sh
+++ .local/bin/master.sh
@@ -2,9 +2,21 @@
 
 # Set variables
 repoDir="$HOME/note"
+readmeFile="$repoDir/README.md"
 journalsDir="$repoDir/journals"
 indexFile="$repoDir/assets/index.html"
 temporaryFile="$HOME/yaoniplan.md"
+fileName=$(date +%F_%H-%M)
+readFromClipboard() {
+    if command -v xclip; then
+        yourClipboard="$(xclip -selection clipboard)"
+    else
+        yourClipboard="$(xsel --output --clipboard)"
+    fi
+}
+sendToTheClipboard() {
+    echo -n "!["$fileName"."$fileExtension"](../assets/"$fileName"."$fileExtension")" | xclip -selection clipboard
+}
 
 # Set functions
 notification () {
