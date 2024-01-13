ae4501543f8ab2c9e64d5434eba86ac07ad19d07
yaoniplan <yaoniplan@gmail.com>
2024-01-14 00:24:21 +0800

    Roll back to the previous version to delete conditional judgement

--- .local/bin/playWithMPV.sh
+++ .local/bin/playWithMPV.sh
@@ -2,8 +2,4 @@
 
 # Dependencies: mpv, yt-dlp, xsel
 
-if [[ "$#" = 2 ]]; then
-    mpv --speed=2 --fullscreen --mute=yes --sub-file="$1" "$2"
-else
-    mpv --speed=2 --fullscreen --mute=yes $(xsel --output --clipboard)
-fi
+mpv --speed=2 --fullscreen --mute=yes $(xsel --output --clipboard)
