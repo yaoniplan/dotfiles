46bdbb82d36dd75f8ecea5daa71f53f42ed46866
yaoniplan <yaoniplan@gmail.com>
2024-02-04 01:50:04 +0800

    Check if the display server protocol is Wayland

--- .local/bin/getDateAndTime.sh
+++ .local/bin/getDateAndTime.sh
@@ -3,17 +3,29 @@
 # Dependencies: notify-send, gtts-cli, mpg123
 
 # Set variables
-currentDate=$(date +%x) # 08/20/2023
-currentTime=$(date +%H:%M) # 10:03
-currentWeekday=$(date +%A) # Sunday
+currentDate=$(date +%x) # e.g. 08/20/2023
+currentTime=$(date +%H:%M) # e.g. 10:03
+currentWeekday=$(date +%A) # e.g. Sunday
 
-# Check if the first argument is coorsponding string
-if [[ "$1"  == "date" ]]; then
-    export $(dbus-launch); notify-send "$currentDate" && gtts-cli "$currentDate" | mpg123 -
-elif [[ "$1" == "time" ]]; then
-    export $(dbus-launch); notify-send "$currentTime" && gtts-cli "$currentTime" | mpg123 -
-elif [[ "$1" == "weekday" ]]; then
-    export $(dbus-launch); notify-send "$currentWeekday" && gtts-cli "$currentWeekday" | mpg123 -
+if [[ "$XDG_SESSION_TYPE" = "wayland" ]]; then
+    if [[ "$1"  == "date" ]]; then
+        notify-send "$currentDate" && gtts-cli "$currentDate" | mpg123 -
+    elif [[ "$1" == "time" ]]; then
+        notify-send "$currentTime" && gtts-cli "$currentTime" | mpg123 -
+    elif [[ "$1" == "weekday" ]]; then
+        notify-send "$currentWeekday" && gtts-cli "$currentWeekday" | mpg123 -
+    else
+        notify-send "$currentTime"; gtts-cli "$currentTime" | mpg123 -
+    fi
 else
-    export $(dbus-launch); notify-send "$currentTime" && gtts-cli "$currentTime" | mpg123 -
+    # Check if the first argument is coorsponding string
+    if [[ "$1"  == "date" ]]; then
+        export $(dbus-launch); notify-send "$currentDate" && gtts-cli "$currentDate" | mpg123 -
+    elif [[ "$1" == "time" ]]; then
+        export $(dbus-launch); notify-send "$currentTime" && gtts-cli "$currentTime" | mpg123 -
+    elif [[ "$1" == "weekday" ]]; then
+        export $(dbus-launch); notify-send "$currentWeekday" && gtts-cli "$currentWeekday" | mpg123 -
+    else
+        export $(dbus-launch); notify-send "$currentTime" && gtts-cli "$currentTime" | mpg123 -
+    fi
 fi
