a83bfeb4df5e2cbf9a11a3b94b25749bc0711cf7
yaoniplan <yaoniplan@gmail.com>
2025-02-06 18:50:56 +0800

    Switch from dmenu to fuzzel for task selection in dmenu-todo.sh
    - Replaced `dmenu` with `fuzzel` for Wayland
    - Updated the task file path to be relative to the script location.

--- .local/bin/dmenu-todo.sh
+++ .local/bin/dmenu-todo.sh
@@ -3,23 +3,26 @@
 # Reference:
 # https://tools.suckless.org/dmenu/scripts/todo
 
-file="$HOME/note/index.md"
+# Directory where the script is located
+SCRIPT_DIR=$(dirname "$0")
+file="$SCRIPT_DIR/todo.txt"
+
 touch "$file"
 height=$(wc -l "$file" | awk '{print $1}')
 prompt="Add/delete a task: "
 
-cmd=$(dmenu -l "$height" -p "$prompt" "$@" < "$file")
+cmd=$(fuzzel --dmenu -l "$height" -p "$prompt" "$@" < "$file")
 while [ -n "$cmd" ]; do
- 	if grep -q "^$cmd\$" "$file"; then
-		grep -v "^$cmd\$" "$file" > "$file.$$"
-		mv "$file.$$" "$file"
-        height=$(( height - 1 ))
- 	else
-		echo "$cmd" >> "$file"
-		height=$(( height + 1 ))
- 	fi
+  if grep -q "^$cmd\$" "$file"; then
+    grep -v "^$cmd\$" "$file" > "$file.$$"
+    mv "$file.$$" "$file"
+    height=$(( height - 1 ))
+  else
+    echo "$cmd" >> "$file"
+    height=$(( height + 1 ))
+  fi
 
-	cmd=$(dmenu -l "$height" -p "$prompt" "$@" < "$file")
+  cmd=$(fuzzel --dmenu -l "$height" -p "$prompt" "$@" < "$file")
 done
 
 exit 0
