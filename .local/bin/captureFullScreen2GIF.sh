08ae25bb6499a0b4b8fe6e6329f24d2e99437b6f
yaoniplan <yaoniplan@gmail.com>
2026-08-14 07:59:05 +0800

    Add Wayland support with wf-recorder backend

--- .local/bin/captureFullScreen2GIF.sh
+++ .local/bin/captureFullScreen2GIF.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
+if [[ "$XDG_SESSION_TYPE" = "wayland" ]]; then
+    # Set variables
+    fileName=$(date +%Y-%m-%dT%H:%M:%SZ).gif
 
-source $HOME/.local/bin/master.sh
+    if ps aux | grep -v grep | grep wf-recorder &>/dev/null; then
+        pkill wf-recorder
+    else
+        wf-recorder -f "$HOME/$fileName" --codec=gif
+    fi
 
-# Delay before starting
-fileExtension="gif"
-DELAY=3
-
-# Duration and output file
-if [ $# -gt 0 ]; then
-    D="--duration=$@"
+    # Record 10 seconds
+    #sleep 10s; pkill -SIGINT wf-recorder
 else
-    echo Default recording duration "$DELAY"s to "$fileName".gif
-    D="--duration=10 "$fileName".gif"
+    source $HOME/.local/bin/master.sh
+
+    # Delay before starting
+    fileExtension="gif"
+    DELAY=3
+
+    # Duration and output file
+    if [ $# -gt 0 ]; then
+        D="--duration=$@"
+    else
+        echo Default recording duration "$DELAY"s to "$fileName".gif
+        D="--duration=10 "$fileName".gif"
+    fi
+    XWININFO=$(xwininfo)
+    read X <<< $(awk -F: '/Absolute upper-left X/{print $2}' <<< "$XWININFO")
+    read Y <<< $(awk -F: '/Absolute upper-left Y/{print $2}' <<< "$XWININFO")
+    read W <<< $(awk -F: '/Width/{print $2}' <<< "$XWININFO")
+    read H <<< $(awk -F: '/Height/{print $2}' <<< "$XWININFO")
+
+    echo Delaying $DELAY seconds. After that, byzanz will start
+    for (( i=$DELAY; i>0; --i )) ; do
+        echo $i
+        sleep 1
+    done
+
+    byzanz-record --verbose --delay=0 --x=$X --y=$Y --width=$W --height=$H $D
+    sendToTheClipboard
 fi
-XWININFO=$(xwininfo)
-read X <<< $(awk -F: '/Absolute upper-left X/{print $2}' <<< "$XWININFO")
-read Y <<< $(awk -F: '/Absolute upper-left Y/{print $2}' <<< "$XWININFO")
-read W <<< $(awk -F: '/Width/{print $2}' <<< "$XWININFO")
-read H <<< $(awk -F: '/Height/{print $2}' <<< "$XWININFO")
-
-echo Delaying $DELAY seconds. After that, byzanz will start
-for (( i=$DELAY; i>0; --i )) ; do
-    echo $i
-    sleep 1
-done
-
-byzanz-record --verbose --delay=0 --x=$X --y=$Y --width=$W --height=$H $D
-sendToTheClipboard
