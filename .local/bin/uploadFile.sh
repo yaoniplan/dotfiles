0db49b007537a82965903452f370907a5ed09d9a
yaoniplan <yaoniplan@gmail.com>
2026-08-23 10:01:03 +0800

    feat(uploadFile): add destination selection and print upload URL
    - Use selectFile.sh for interactive file selection
    - Prompt for Public or Private destination via tofi
    - Print resulting URL after upload

--- .local/bin/uploadFile.sh
+++ .local/bin/uploadFile.sh
@@ -6,7 +6,7 @@ if [[ "$#" -ne 0 ]]; then
 else
     # Interact with tofi
     if command -v tofi &>/dev/null; then
-        fileName=$(echo "" | tofi)
+        fileName=$(selectFile.sh)
     else
         echo "Usage: $0 [file name]"
         exit 1
@@ -21,14 +21,30 @@ fi
 
 # Adjust based on needs
 authorization="admin:jj"
-filePath="chinaTelecom/$(date +%Y/%m/%d)"
-endpoint="http://100.65.173.16:5244/dav/"$filePath"/"
+selected_option=$(echo -e "Public\nPrivate" | tofi)
+
+case "$selected_option" in
+    "Public")
+        filePath="chinaTelecom/$(date +%Y/%m/%d)"
+        endpoint="http://100.65.173.16:5244/dav/"$filePath"/"
+        ;;
+    "Private")
+        filePath="grow/temporary/$(date +%Y/%m/%d)"
+        endpoint="http://100.65.173.16:5244/dav/"$filePath"/"
+        ;;
+    *)
+        echo "Invalid selection. Aborting."
+        exit 1
+        ;;
+esac
 
 curl --user "$authorization" --upload-file "$fileName" "$endpoint"
 
 # Notify with notify-send or terminal
 if command -v notify-send; then
     notify-send "Upload" "$fileName"
+    echo $(echo "$endpoint"$(basename "$fileName") | sed 's/\/dav//g')
 else
     echo "Upload" "$fileName"
+    echo $(echo "$endpoint"$(basename "$fileName") | sed 's/\/dav//g')
 fi
